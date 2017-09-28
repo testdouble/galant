05dcdf5775dd828b5d465b92351f1ff8078a9337
Schoonology <michael.r.schoonmaker@gmail.com>
2017-09-28 09:33:30 -0700

    Add SimpleWebRTC.

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,31 @@
-export default () => <div><h1>Galant</h1></div>
+import SimpleWebRTC from 'simplewebrtc'
+import React from 'react'
+
+export default class Index extends React.Component {
+  componentDidMount () {
+    var webrtc = new SimpleWebRTC({
+      localVideoEl: this.refs.localVideo,
+      remoteVideosEl: this.refs.remoteVideos,
+      autoRequestMedia: true
+    })
+
+    webrtc.on('readyToCall', function () {
+      webrtc.joinRoom('galant')
+
+      setTimeout(() => {
+        webrtc.leaveRoom()
+        webrtc.joinRoom('galant2')
+      }, 5000)
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Galant</h1>
+        <video ref='localVideo' />
+        <div ref='remoteVideos' />
+      </div>
+    )
+  }
+}
